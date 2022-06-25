057162f9c925848e03d10cf9c8dec6f81f0295dd
fbiletsiou <florabiletsiou@gmail.com>
2022-06-25 22:27:22 +0100

    signin working

--- screens/SignIn.js
+++ screens/SignIn.js
@@ -1,11 +1,27 @@
-import React, { useContext } from "react";
-import { View, Text } from "react-native";
+import { async } from "@firebase/util";
+import React, { useContext, useState } from "react";
+import { View, Text, Image, TextInput, Button } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 import Context from "../context/Context";
+import { signIn, signUp } from "../firebase";
 
 export default function SignIn() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [mode, setMode] = useState("signUp");
+
     const {
         theme: {colors}
     } = useContext(Context);
+
+    async function handlePress() {
+        if(mode === "signUp"){
+            await signUp(email, password);
+        }
+        if( mode === "signIn") {
+            await signIn(email, password);
+        }
+    }
     return (
         <View 
             style={{
@@ -20,8 +36,52 @@ export default function SignIn() {
                     fontSize: 24, 
                     marginBottom: 20}}
             >
-                Welcome to whatsapp 
+                Welcome to Whatsapp 
             </Text>
+            <Image source={require("../assets/welcome-img.png")} style={{width:180, height:180}} resizeMode= "cover" />
+            
+            <View style={{marginTop: 20}}>
+                <TextInput 
+                    placeholder="Email" 
+                    value={email}
+                    onChangeText={setEmail} 
+                    style={{
+                        borderBottomColor: 
+                        colors.primary, 
+                        borderBottomWidth: 2, 
+                        width: 200}} 
+                />
+                <TextInput 
+                    placeholder="Password"
+                    value={password}
+                    onChangeText={setPassword} 
+                    secureTextEntry={true} 
+                    style={{
+                        borderBottomColor: colors.primary, 
+                        borderBottomWidth: 2, 
+                        width: 200, 
+                        marginTop: 20}} 
+                />
+
+                <View style={{marginTop: 20}}>
+                    <Button 
+                        title={mode === "signUp" ? "Sign Up": "Sign in"} 
+                        disabled={!password || !email}
+                        onPress={handlePress} 
+                        color={colors.secondary} 
+                    />
+                </View>
+                <TouchableOpacity 
+                    onPress={() => 
+                        mode === "signUp" ? setMode("signIn"): setMode("signUp")} 
+                    style={{marginTop:15}}
+                >
+                    <Text style={{color: colors.secondaryText}}>{mode === "signUp" ? "Already have an account? Sign in": "Don't have an account? Sign Up"}</Text>
+
+                </TouchableOpacity>
+
+            </View>
+
         </View>
     )
 }
