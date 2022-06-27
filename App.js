30981b65ee5d15744c93db97e62fd118b8fd6ac2
fbiletsiou <florabiletsiou@gmail.com>
2022-06-27 22:25:07 +0100

    signup completed (name and picture)

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import { Text, View, LogBox } from 'react-native';
 import {useAssets} from "expo-asset";
 import { onAuthStateChanged } from "firebase/auth";
@@ -7,6 +7,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import SignIn from "./screens/SignIn";
 import ContextWrapper from './context/ContextWrapper';
+import Context from './context/Context';
+import Profile from "./screens/Profile";
 
 LogBox.ignoreLogs([
   "Setting a timer",
@@ -18,6 +20,7 @@ const Stack = createStackNavigator();
 function App() {
   const [currUser, setCurrUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const {theme: {colors}} = useContext(Context);
   
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -40,12 +43,35 @@ function App() {
                 <Stack.Screen name='signIn' component={SignIn} />
         </Stack.Navigator>
 
-      ): <Text>Hi user!</Text>}
+      ): (
+        <Stack.Navigator 
+          screenOptions={{
+            headerStyle:{
+              backgroundColor: colors.foreground,
+              shadowOpacity: 0,
+              elevation: 0,
+            },
+            headerTintColor: colors.white,
+          }}
+        >
+          {!currUser.displayName && (
+          <Stack.Screen 
+            name='profile' 
+            component={Profile} 
+            options={{headerShown: false}} 
+          />
+          )}
+          <Stack.Screen name="home" options={{title: "Whatsapp"}} component={Home} />
+        </Stack.Navigator>
 
+      )}
     </NavigationContainer>
   );
 }
 
+function Home() {
+  return <Text> Hi, I have a profile!</Text>;
+}
 
 function Main() {
   const [assets] = useAssets(
--- ReadMe.md
+++ ReadMe.md
@@ -1,5 +1,6 @@
 # Whatsapp Clone Project
-https://youtu.be/YPSjNIJEdXU?t=4075 
+
+https://youtu.be/YPSjNIJEdXU?t=7223
 
 Practice Project following the [Esteban Codes Tutorial](https://www.youtube.com/watch?v=YPSjNIJEdXU)
 
