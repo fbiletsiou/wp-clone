fca0a9a1dbdd7bc24732d007329e751cff2fb6a3
fbiletsiou <florabiletsiou@gmail.com>
2022-07-01 22:01:02 +0100

    contact avatar creation

--- screens/Contacts.js
+++ screens/Contacts.js
@@ -1,10 +1,51 @@
-import React from "react";
-import { View, Text } from "react-native";
+import { collection, onSnapshot, query, where } from "@firebase/firestore";
+import { useRoute } from "@react-navigation/native";
+import React, { useContext, useEffect, useState } from "react";
+import { View, Text, FlatList } from "react-native";
+import ListItem from "../components/ListItem";
+import GlobalContext from "../context/Context";
+import { db } from "../firebase";
+import useContacts from "../hooks/useHooks";
 
 export default function Contacts() {
+    const contacts = useContacts();
+    const route = useRoute();
+    const image = route.params && route.params.image;
+    
     return(
-        <View>
-            <Text>Contacts</Text>
-        </View>
-    )
+        <FlatList 
+            style={{flex:1, padding: 10}} 
+            data={contacts} 
+            keyExtractor={(_, i) => i}
+            renderItem={({item}) => <ContactPreview contact={item} image={image} />} 
+        />
+    );
+}
+
+function ContactPreview({contact, image}) {
+    const {rooms} = useContext(GlobalContext);
+    const [user, setUser] = useState(contact);
+
+    useEffect(() => {
+        const q = query(
+            collection(db, "users"),
+            where("email", "==",  contact.email)
+        );
+        const unsubscribe = onSnapshot(q, (snapshot) => {
+            if(snapshot.docs.length) {
+                const userDoc = snapshot.docs[0].data();
+                setUser((prevUser) => ({...prevUser, userDoc}));
+            }
+        });
+        return () => unsubscribe();
+    }, []);
+    return (
+        <ListItem 
+            style={{marginTop: 7}} 
+            type="contacts" 
+            user={user} 
+            image={image} 
+            room={rooms.find((room) => rooms.participantsArray.includes(contact.email))} 
+        />
+    );
 }
